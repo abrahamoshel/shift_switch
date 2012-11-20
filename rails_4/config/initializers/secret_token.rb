804c701c94ec4c5edd4e1cdb4aa9566657e038ba
Abraham Oshel <abraham.oshel@gmail.com>
2012-11-20 17:23:43 -0600

    adding rails4 and sinatra examples

---
+++ rails_4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4::Application.config.secret_key_base = 'f64e7505a93182596fa1b5bb21a88b6e17ce369f0f7cecb2864b102d79ce7a9f669c45b1c5c554b482d2a5bb08632e01862debea04802f2c6f17366c9a4e6fee'
